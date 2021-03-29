86c31c19eb83863f737d53dbaa650f68567507ad
nstuivenbergnovi <n.stuivenberg@novi.nl>
2021-03-29 13:49:58 +0200

    Readme, Main en gitignore

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Wat willen jullie behandelen?
+
+ * 
---
+++ src/nl/novi/uitleg/Main.java
@@ -0,0 +1,8 @@
+package nl.novi.uitleg;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
